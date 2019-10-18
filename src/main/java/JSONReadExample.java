1810459cc5fe293fc20e9e39992de66642a516ed
Anass-ELGHAOUI <elghaoui.anass@gmail.com>
2019-10-18 16:57:10 +0200

    adding lombok dependency

--- src/main/java/JSONReadExample.java
+++ src/main/java/JSONReadExample.java
@@ -1,20 +1,35 @@
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 import java.io.FileReader;
-  
+import java.util.Iterator;
+import java.util.Map;
+
 public class JSONReadExample  
 { 
     public static void main(String[] args) throws Exception  
     { 
         // parsing file "JSONExample.json" 
-        Object obj = new JSONParser().parse(new FileReader("JSONExample.json")); 
+        Object obj = new JSONParser().parse(new FileReader("C:/Users/Anass/Documents/JSONExample.json"));
           
         // typecasting obj to JSONObject 
         JSONObject jo = (JSONObject) obj; 
           
-        // getting firstName and lastName 
-        String firstName = (String) jo.get("firstName"); 
-        String lastName = (String) jo.get("lastName"); 
+        // getting type and name
+        String typex = (String) jo.get("type");
+        String namex = (String) jo.get("name");
+
+        // getting features
+        JSONArray ja = (JSONArray) jo.get("features");
+
+        // iterating features
+        Iterator itr2 = ja.iterator();
+
+        while (itr2.hasNext())
+        {
+            Map feature = ((Map) itr2.next());
+            System.out.println("type : "+feature.get("type"));
+        }
     }
 }
