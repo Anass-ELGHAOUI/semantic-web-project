e9b1f0f5e8c47f87ef368cd8099ee682ea5f2022
Anass-ELGHAOUI <elghaoui.anass@gmail.com>
2019-10-25 14:03:30 +0200

    adding lombok plugin and data notation

--- src/main/java/models/City.java
+++ src/main/java/models/City.java
@@ -1,27 +1,15 @@
 package models;
 
+import lombok.Data;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Data
 public class City {
     private String name;
     private List<BikeStation> bikeStations = new ArrayList<BikeStation>();
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public List<BikeStation> getBikeStations() {
-        return bikeStations;
-    }
-
-    public void setBikeStations(List<BikeStation> bikeStations) {
-        this.bikeStations = bikeStations;
-    }
 
     public void addBikeStation(BikeStation bikeStation) {
         this.bikeStations.add(bikeStation);
