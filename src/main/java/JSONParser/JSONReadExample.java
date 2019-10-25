8bdb8b6ce157300a5c3ca9f170e209c6c7b38e17
Anass-ELGHAOUI <elghaoui.anass@gmail.com>
2019-10-25 14:39:27 +0200

    parsing json data from an url

--- src/main/java/JSONParser/JSONReadExample.java
+++ src/main/java/JSONParser/JSONReadExample.java
@@ -7,15 +7,26 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 import java.io.FileReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Iterator;
 import java.util.Map;
 
 public class JSONReadExample
 { 
     public static void main(String[] args) throws Exception  
-    { 
+    {
+        String sURL = "https://download.data.grandlyon.com/wfs/rdata?SERVICE=WFS&VERSION=1.1.0&outputformat=GEOJSON&request=GetFeature&typename=jcd_jcdecaux.jcdvelov&SRSNAME=urn:ogc:def:crs:EPSG::4171"; //just a string
+
+        // Connect to the URL using java's native library
+        URL url = new URL(sURL);
+        URLConnection request = url.openConnection();
+        request.connect();
+
         // parsing file "JSONExample.json" 
-        Object obj = new JSONParser().parse(new FileReader("C:/Users/Anass/Documents/JSONExample.json"));
+        Object obj = new JSONParser().parse(new InputStreamReader((InputStream) request.getContent()));
 
         City lyon = new City();
         lyon.setName("Lyon");
@@ -48,6 +59,19 @@ public class JSONReadExample
             bikeStation.setFree((String)properties.get("available_bike_stands"));
             bikeStation.setCardPaiement((String)properties.get("banking"));
             lyon.addBikeStation(bikeStation);
+        }
+
+        for (BikeStation bikeStation : lyon.getBikeStations()){
+            System.out.println("Name : "+bikeStation.getName());
+            System.out.println("Id : "+bikeStation.getId());
+            System.out.println("Lattitude : "+bikeStation.getLattitude());
+            System.out.println("Longtitude : "+bikeStation.getLongitude());
+            System.out.println("Toal : "+bikeStation.getTotal());
+            System.out.println("Available : "+bikeStation.getAvailable());
+            System.out.println("Free : "+bikeStation.getFree());
+            System.out.println("Card Paiement : "+bikeStation.getCardPaiement()+"\n\n");
+
+
         }
     }
 }
