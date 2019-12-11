3bdd5be4c21c4d854c8fe5064dd5fc3f37b8aca0
walid.ouchtiti <walid.ouchtiti@etu.emse.fr>
2019-12-11 11:36:54 +0100

    add pagination

--- semantic-web-project-web-app/js/rdf.js
+++ semantic-web-project-web-app/js/rdf.js
@@ -1,3 +1,29 @@
+/* Taken from stackoverflom */
+function pagination (NumberOfRows) {
+    $('#table').after('<div id="nav"></div>');
+    var rowsShown = 5;
+    var rowsTotal = NumberOfRows;
+    var numPages = rowsTotal/rowsShown;
+
+    for(i = 0;i < numPages;i++) {
+        var pageNum = i + 1;
+        $('#nav').append('<a href="#" rel="'+i+'">'+pageNum+'</a> ');
+    }
+    $('#table tbody tr').hide();
+    $('#table tbody tr').slice(0, rowsShown).show();
+    $('#nav a:first').addClass('active');
+    $('#nav a').bind('click', function(){
+
+        $('#nav a').removeClass('active');
+        $(this).addClass('active');
+        var currPage = $(this).attr('rel');
+        var startItem = currPage * rowsShown;
+        var endItem = startItem + rowsShown;
+        $('#table tbody tr').css('opacity','0.0').hide().slice(startItem, endItem).
+        css('display','table-row').animate({opacity:1}, 300);
+    });
+}
+
 function getAllCountries() {
     var query = "PREFIX dbo: <http://dbpedia.org/ontology/>" +
                  "SELECT DISTINCT ?country WHERE {" +
@@ -278,7 +304,10 @@ function getCityInfo() {
 
                 table.appendChild(bikeStation);
             }
+
+            pagination(bindings.length);
         });
+
     }
 }
 
